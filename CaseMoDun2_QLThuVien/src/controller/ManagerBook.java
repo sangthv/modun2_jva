8c3e101c8fd931bb04912da1219e744d34509d59
sangpk <dinhsangcnc@gmail.com>
2022-10-06 10:03:09 +0700

    modun2

--- CaseMoDun2_QLThuVien/src/controller/ManagerBook.java
+++ CaseMoDun2_QLThuVien/src/controller/ManagerBook.java
@@ -10,53 +10,85 @@ import java.util.Comparator;
 import java.util.Scanner;
 
 public class ManagerBook {
-File file = new File("E:\\modun2_java\\modun2\\CaseMoDun2_QLThuVien\\src\\FileText\\Book.txt");
-ReadAndWrite <Book> readAndWrite = new ReadAndWrite<>();
-ArrayList<Book> books = readAndWrite.read(file);
-Scanner scanner = new Scanner(System.in);
+    File file = new File("E:\\modun2_java\\modun2\\CaseMoDun2_QLThuVien\\src\\FileText\\Book.txt");
+    ReadAndWrite<Book> readAndWrite = new ReadAndWrite<>();
+    ArrayList<Book> books = readAndWrite.read(file);
+    Scanner scanner = new Scanner(System.in);
 
-public Book inPut() {
-    System.out.println(" nhập tên sách :");
-    String bookName = scanner.nextLine();
-    System.out.println(" nhập tên tác giả: ");
-    String authorName = scanner.nextLine();
-    System.out.println(" nhập ngày xuất bản : ");
-    String publishDate = scanner.nextLine();
-    System.out.println(" nhập nhà sản xuất: ");
-    String manufactureName = scanner.nextLine();
-    int price = 0;
-    do {
-        try {
-            System.out.println(" nhập giá: ");
-            price = Integer.parseInt(scanner.nextLine());
+    public Book inPut() {
+        System.out.println(" nhập tên sách :");
+        String bookName = scanner.nextLine();
+        System.out.println(" nhập tên tác giả: ");
+        String authorName = scanner.nextLine();
+        System.out.println(" nhập ngày xuất bản : ");
+        String publishDate = scanner.nextLine();
+        System.out.println(" nhập nhà sản xuất: ");
+        String manufactureName = scanner.nextLine();
+        int price = 0;
+        do {
+            try {
+                System.out.println(" nhập giá: ");
+                price = Integer.parseInt(scanner.nextLine());
 
-        } catch (Exception e) {
-            System.out.println("Bạn phải nhập số!");
-        }
-    }while (price == 0);
+            } catch (Exception e) {
+                System.out.println("Bạn phải nhập số!");
+            }
+        } while (price == 0);
 
-    return new Book(bookName, authorName, publishDate, manufactureName, price);
-}
-    public void addBook(Book book){
+        return new Book(bookName, authorName, publishDate, manufactureName, price);
+    }
+
+    public void addBook(Book book) {
         books.add(book);
-        readAndWrite.write(file,books);
+        readAndWrite.write(file, books);
     }
 
-public void show(){
-    for (int i = 0; i<books.size(); i++ ){
-        System.out.println(books.get(i).toString());
-    }
-}
-public void sortByAuthorName(){
-    Collections.sort(books, new Comparator<Book>() {
-        @Override
-        public int compare(Book o1, Book o2) {
-            return o1.getAuthorName().compareToIgnoreCase(o2.getAuthorName());
+    public void show() {
+        for (int i = 0; i < books.size(); i++) {
+            System.out.println(books.get(i).toString());
         }
-    });
-}
-    public void delete(){
-     books.clear();
-        readAndWrite.write(file,books);
+    }
+
+    //sắp xếp sách theo tên tác giả............................
+    public void sortByAuthorName() {
+        Collections.sort(books, new Comparator<Book>() {
+            @Override
+            public int compare(Book o1, Book o2) {
+                return o1.getAuthorName().compareToIgnoreCase(o2.getAuthorName());
+            }
+        });
+    }
+
+    //tìm kiếm theo tên sách...................
+    public Book findBook() {
+        while (true) {
+            System.out.println("Nhập tên sách: ");
+            String bookName = scanner.nextLine();
+            for (int i = 0; i < books.size(); i++) {
+                if (books.get(i).getBookName().equals(bookName)) {
+                    System.out.println("Đã thấy!");
+                    return books.get(i);
+                }
+            }
+            System.out.println("nhập sai");
+        }
+    }
+
+    //sửa giá.......................
+    public void editPrice() {
+        Book b = findBook();
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).getBookName().equals(b.getBookName())) {
+                System.out.println("nhập lại giá muốn sửa: ");
+                int price = Integer.parseInt(scanner.nextLine());
+                books.get(i).setPrice(price);
+            }
+        }
+    }
+
+    //xóa tất cả danh sách.................
+    public void delete() {
+        books.clear();
+        readAndWrite.write(file, books);
     }
 }
--- CaseMoDun2_QLThuVien/src/views/MenuBook.java
+++ CaseMoDun2_QLThuVien/src/views/MenuBook.java
@@ -7,39 +7,46 @@ import java.util.Scanner;
 
 public class MenuBook {
 
-public void menu (){
+    public void menu() {
         Scanner scanner = new Scanner(System.in);
-    ManagerBook managerBook = new ManagerBook();
-        int choice ;
+        ManagerBook managerBook = new ManagerBook();
+        int choice;
         do {
-        System.out.println(" Quản Lí Thư Viện Sách: ");
-        System.out.println(" 1. Nhập thông tin quấn sách");
-        System.out.println(" 2. Hiền thị thông tin sách");
-        System.out.println(" 3. Sắp xếp theo tên tác giả");
-        System.out.println(" 4. sửa thông tin sách theo giá");
-        System.out.println(" 5. xóa thông tin sách");
-        System.out.println(" 0. thoát ");
-       choice = Integer.parseInt(scanner.nextLine());
-        switch (choice){
-            case 1:
-                Book book = managerBook.inPut();
-                managerBook.addBook(book);
-                break;
-            case 2:
-                managerBook.show();
-                break;
-            case 3:
-                managerBook.sortByAuthorName();
-                break;
-//            case 4:
-//
-//                break;
-            case 5:
-                managerBook.delete();
-                break;
-            default:
-                System.out.println("cảm ơn bạn đã đến thư viện sách");
-        }
-    }while (choice !=0);
+            System.out.println("--------------------------------------------------");
+            System.out.println(" Quản Lí Thư Viện Sách: ");
+            System.out.println(" 1. Nhập thông tin quấn sách");
+            System.out.println(" 2. Hiền thị thông tin sách");
+            System.out.println(" 3. Sắp xếp theo tên tác giả");
+            System.out.println(" 4. Tìm kiếm tên sách");
+            System.out.println(" 5. sửa thông tin sách theo giá");
+            System.out.println(" 6. xóa thông tin sách");
+            System.out.println(" 0. thoát ");
+            System.out.println("------------------------------------------------------");
+            choice = Integer.parseInt(scanner.nextLine());
+            switch (choice) {
+                case 1:
+                    Book book = managerBook.inPut();
+                    managerBook.addBook(book);
+                    break;
+                case 2:
+                    managerBook.show();
+                    break;
+                case 3:
+                    managerBook.sortByAuthorName();
+                    break;
+                case 4:
+                    Book book1 = managerBook.findBook();
+                    System.out.println(book1);
+                    break;
+                case 5:
+                    managerBook.editPrice();
+                    break;
+                case 6:
+                    managerBook.delete();
+                    break;
+                default:
+                    System.out.println("cảm ơn bạn đã đến thư viện sách");
+            }
+        } while (choice != 0);
     }
 }
